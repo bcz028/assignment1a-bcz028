6bf004c6a0bf1c16b9ab83892039f64b09311c87
Ben Zhao <112284084+bcz028@users.noreply.github.com>
2024-09-13 16:02:00 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "bcz028";
+    char STUDENT_ID[] = "985298694";
 
     return 0 ;
 }
